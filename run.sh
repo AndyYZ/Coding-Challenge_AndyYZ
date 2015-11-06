3120c65277ae33f5fcad226769883ee32a1fb6b4
Andrew <AndyYZ@users.noreply.github.com>
2015-11-05 19:31:27 -0600

    Update

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
 #Run Environment: Java SE 6
+javac -cp .:lib/json-20090211.jar src/CodeChallengeAZ.java
 java -cp .:lib/json-20090211.jar src/CodeChallengeAZ tweet_output/ft1.txt tweet_output/ft2.txt tweet_input/tweets.txt 
