67bff58e62bbcee8f8e6be6908f94af285f469bf
Andrew <AndyYZ@users.noreply.github.com>
2015-11-05 18:16:53 -0600

    First commit

---
+++ run.sh
@@ -0,0 +1,3 @@
+# I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
+#Run Environment: Java SE 6
+java -cp .:lib/json-20090211.jar src/CodeChallengeAZ tweet_output/ft1.txt tweet_output/ft2.txt tweet_input/tweets.txt 
